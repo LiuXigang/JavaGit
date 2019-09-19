dd1a0ddd41b5867374ab77c2431876846f856710
榴莲 <xigang.liu@51hgp.com>
2019-09-19 11:24:09 +0800

    testCommit2

--- GitTest/src/Hello.java
+++ GitTest/src/Hello.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("HelloGit");
         System.out.println("HelloJava");
+        List<Integer> list= Arrays.asList(1,2,3,4,5,6);
+        Collections.shuffle(list);
+        System.out.println(list);
     }
 }

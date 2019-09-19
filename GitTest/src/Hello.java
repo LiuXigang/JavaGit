bef020efe0e240af7a9ea2a7492d503a9c4d0986
榴莲 <xigang.liu@51hgp.com>
2019-09-19 11:43:41 +0800

    newbranceCommit1

--- GitTest/src/Hello.java
+++ GitTest/src/Hello.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -6,7 +7,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("HelloGit");
         System.out.println("HelloJava");
-        List<Integer> list= Arrays.asList(1,2,3,4,5,6);
+        List<Integer> list= new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+        list.add(7);
+        System.out.println(list);
         Collections.shuffle(list);
         System.out.println(list);
     }
